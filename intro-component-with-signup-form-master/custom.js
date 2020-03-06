fb0da6d17344199130bd5b8ca25d7688b3c0a1e3
Velina Petrova <velinarp@gmail.com>
2020-03-06 18:36:18 +0800

    submit styles

---
+++ intro-component-with-signup-form-master/custom.js
@@ -0,0 +1,9 @@
+let firstName = document.querySelector("input[name='firstName']");
+let lastName = document.querySelector("input[name='lastName']");
+let email = document.querySelector("input[name='email']");
+let password = document.querySelector("input[name='password']");
+let claimBtn = document.querySelector("input[type='button']")
+
+claimBtn.addEventListener('click', () => {
+  
+})
