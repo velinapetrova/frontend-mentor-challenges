a4ea0ac2a1e05a1931008128c3b6ee7413107e8e
Velina Petrova <velinarp@gmail.com>
2020-03-06 23:54:55 +0800

    Add behaviour and validations

--- intro-component-with-signup-form-master/custom.js
+++ intro-component-with-signup-form-master/custom.js
@@ -1,9 +1,18 @@
-let firstName = document.querySelector("input[name='firstName']");
-let lastName = document.querySelector("input[name='lastName']");
-let email = document.querySelector("input[name='email']");
-let password = document.querySelector("input[name='password']");
+let inputFields = document.querySelectorAll(".input-field");
 let claimBtn = document.querySelector("input[type='button']")
 
 claimBtn.addEventListener('click', () => {
-  
+  inputFields.forEach((input) => {
+    if (input.validity.valid) return;
+    input.classList.add("invalid");
+    input.nextElementSibling.classList.remove("hidden");
+    input.nextElementSibling.firstElementChild.innerHTML = input.validationMessage;
+  })
+})
+
+inputFields.forEach((input) => {
+  input.addEventListener('click', () => {
+    input.classList.remove("invalid");
+    input.nextElementSibling.classList.add("hidden");
+  })
 })
